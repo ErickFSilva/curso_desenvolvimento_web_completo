a8266cdd54ba4c4bc64abe235da60d359d98e2cc
ErickFSilva <erick_ferreira@outlook.com>
2023-08-04 16:53:21 -0300

    Alterações na seção 11, 04/08/2023 (tarde)

--- secao-11-php7/aula331-funcoes-nativas-tarefas-matematicas.php
+++ secao-11-php7/aula331-funcoes-nativas-tarefas-matematicas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,56 +10,86 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
     <style>
-        ul, ol { margin: 20px 0; }
-        dt { margin: 10px 0; }
-        dd { text-indent: 20px; }
+        section {
+            margin: 20px 0;
+        }
+
+        dt, li {
+            margin: 10px 0;
+        }
+
+        dd {
+            text-indent: 20px;
+        }
     </style>
 </head>
+
 <body>
 
-<div class="container py-3">
+    <div class="container py-3">
 
-    <header>
-        <h1 class="h3 text-center">Funções nativas para tarefas matemáticas</h1>
-    </header>
+        <header>
+            <h1 class="h3 text-center">Funções nativas para tarefas matemáticas</h1>
+        </header>
 
-    <ul>
-        <li><b>ceil($numero)</b>: Arredonda o valor para cima</li>
-        <li><b>floor($numero)</b>: Arredonda o valor para baixo</li>
-        <li><b>round($numero, casas-pos-ponto)</b>: Arredonda o valor com base nas casas decimais</li>
-        <li><b>rand()</b>: Gera um inteiro aleatório</li>
-        <li><b>rand(inicio, fim)</b>: Gera um inteiro com base nos parâmetros informados</li>
-        <li><b>sqrt($numero)</b>: Retorna a raiz quadrada</li>
-    </ul>
-    <hr>
+        <section>
+            <ul>
+                <li><b>ceil($numero)</b>: Arredonda o valor para cima</li>
+                <li><b>floor($numero)</b>: Arredonda o valor para baixo</li>
+                <li><b>round($numero, casas-pos-ponto)</b>: Arredonda o valor com base nas casas decimais</li>
+                <li><b>rand()</b>: Gera um inteiro aleatório de 0 até randmax
+                    <ul>
+                        <li><b>getrandmax()</b>: retorna o maior valor aleatório possível com base no sistema operacional</li>
+                    </ul>
+                </li>
+                <li><b>rand(inicio, fim)</b>: Gera um inteiro com base nos parâmetros informados</li>
+                <li><b>sqrt($numero)</b>: Retorna a raiz quadrada</li>
+            </ul>
+        </section>
+        <hr>
 
-    <?php
+        <?php
 
-        $numero = 35.854;
-        echo $numero.'<br><br>';
+        // Variáveis
+        $num1 = 7.36;
+        $num2 = 8.54;
 
-        // ceil - Arredonda o valor para cima
-        echo ceil($numero).'<br>';
-
-        // floor - Arredonda o valor para baixo
-        echo floor($numero).'<br>';
-
-        // round - Arredonda o valor com base nas casas decimais
-        echo round($numero, 1).'<br>';
-
-        // rand - Gera um inteiro aleatório
-        echo rand().'<br>';
-        echo getrandmax().'<br>';
-
-        // sqrt - Retorna a raiz quadrada
-        echo sqrt(81);
-
-    ?>
-
-</div>
+        echo '<p>Num1: ' . $num1 . '</p>';
+        echo '<p>Num2: ' . $num2 . '</p>';
+        echo '<hr>';
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
+        // ceil()?
+        echo '<p>ceil(): ' . ceil($num1) . '</p>';
+        echo '<p>ceil(): ' . ceil($num2) . '</p>';
+
+
+        // floor():
+        echo '<p>ceil(): ' . floor($num1) . '</p>';
+        echo '<p>ceil(): ' . floor($num2) . '</p>';
+
+
+        // round():
+        echo '<p>round(): ' . round($num1) . '</p>';
+        echo '<p>round(): ' . round($num2, 2) . '</p>';
+
+
+        // rand():
+        echo '<p>rand(): ' . rand() . '</p>';
+        echo '<p>getrandmax(): ' . getrandmax() . '</p>';
+        echo '<p>rand(10, 20): ' . rand(10, 20) . '</p>';
+
+
+        // sqrt():
+        echo '<p>sqrt(): ' . sqrt(81) . '</p>';
+
+        ?>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
--- secao-11-php7/aula332-funcoes-nativas-manipular-datas.php
+++ secao-11-php7/aula332-funcoes-nativas-manipular-datas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,85 +10,71 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
     <style>
-        ul, ul ul, ol { margin: 20px 0; }
-        dt { margin: 10px 0; }
-        dd { text-indent: 20px; }
+        ul,
+        ul ul,
+        ol {
+            margin: 20px 0;
+        }
+
+        dt {
+            margin: 10px 0;
+        }
+
+        dd {
+            text-indent: 20px;
+        }
     </style>
 </head>
+
 <body>
 
-<div class="container py-3">
+    <div class="container py-3">
 
-    <header>
-        <h1 class="h3 text-center">Funções nativas para tarefas matemáticas</h1>
-    </header>
+        <header>
+            <h1 class="h3 text-center">Funções nativas para tarefas matemáticas</h1>
+        </header>
 
-    <ul>
-        <li><b>date(formato)</b>: recuperar a data atual
-            <ul>
-                <li><b>d</b>: dia no formato numérico</li>
-                <li><b>D</b>: dia no formato de sigla</li>
-                <li><b>m</b>: mês no formato numérico</li>
-                <li><b>M</b>: mês no formato de sigla</li>
-                <li><b>y</b>: ano no formato numérico (##)</li>
-                <li><b>Y</b>: ano no formato numérico (####)</li>
-                <li><b>h</b>: hora no formato 12</li>
-                <li><b>H</b>: hora no formato 24</li>
-                <li><b>i</b>: minutos</li>
-                <li><b>s</b>: segundos</li>
-            </ul>
-        </li>
-        <li><b>date_default_timezone_get(timezone)</b>: recuperar o timezone default da aplicação</li>
-        <li><b>date_default_timezone_set(timezone)</b>: atualizar o timezone default da aplicação</li>
-        <li><b>strtotime(data)</b>: Transformar datas textuais em segundos</li>
-    </ul>
-    <hr>
+        <ul>
+            <li><b>date(formato)</b>: recuperar a data atual
+                <ul>
+                    <li><b>d</b>: dia no formato numérico</li>
+                    <li><b>D</b>: dia no formato de sigla</li>
+                    <li><b>m</b>: mês no formato numérico</li>
+                    <li><b>M</b>: mês no formato de sigla</li>
+                    <li><b>y</b>: ano no formato numérico (##)</li>
+                    <li><b>Y</b>: ano no formato numérico (####)</li>
+                    <li><b>h</b>: hora no formato 12</li>
+                    <li><b>H</b>: hora no formato 24</li>
+                    <li><b>i</b>: minutos</li>
+                    <li><b>s</b>: segundos</li>
+                </ul>
+            </li>
+            <li><b>date_default_timezone_get(timezone)</b>: recuperar o timezone default da aplicação</li>
+            <li><b>date_default_timezone_set(timezone)</b>: atualizar o timezone default da aplicação</li>
+            <li><b>strtotime(data)</b>: Transformar datas textuais em segundos</li>
+        </ul>
+        <hr>
 
-    <?php
+        <?php
 
-        // recupera o timezone
-        echo date_default_timezone_get().'<br>';
+        // Recuperar o timezone default da aplicação
+        echo '<p>Timezone default: '.date_default_timezone_get().'</p>';
 
-        // recuperação da data atual, data corrente
-        echo date('Y-m-d H:i').'<br>';
+        // Recuperando a data atual / data corrente
+        echo '<p>Data/hora atual: '.date('d/m/Y H:i') . '</p>';
+        echo '<hr>';
 
-        // seta o timezone
-        date_default_timezone_set('Europe/Berlin');
+        // Atualizar o timezone default da aplicação
+        date_default_timezone_set('Europe/Moscow');
+        echo '<p>Timezone atual: '.date_default_timezone_get().'</p>';
+        echo '<p>Data/hora atual: '.date('d/m/Y H:i').'</p>';
 
-        // 
-        echo '<br>';
-        echo date_default_timezone_get().'<br>';
-        echo date('Y-m-d H:i').'<br>';
+        ?>
 
-    ?>
-    <hr>
+    </div>
 
-    <?php
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-        $data_inicial = '2022-10-24';
-        $data_final = '2022-10-30';
-
-        // timestamp tem como referência, ponto de partida, a data (01/01/1970)
-
-        $time_inicial = strtotime($data_inicial); // retorno em segundos
-        echo $data_inicial.' = '.$time_inicial;
-        echo '<br>';
-        
-        $time_final = strtotime($data_final); // retorno em segundos
-        echo $data_final.' = '.$time_final;
-        echo '<br><br>';
-
-        $dia_time = 24 * 60 * 60;
-        $diferenca_times = $time_final - $time_inicial;
-        
-        echo 'Diferença entre as duas datas: '.($diferenca_times/$dia_time);
-
-    ?>
-
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
 </body>
+
 </html>

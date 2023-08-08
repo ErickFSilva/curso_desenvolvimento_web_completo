a80b41623dcb17bd325841830ed9db23bb0ca1c0
ErickFSilva <erick_ferreira@outlook.com>
2023-08-08 16:46:20 -0300

    Alterações na seção 11, 08/08/2023 (tarde)

--- secao-11-php7/aula336-array-metodos-pesquisa.php
+++ secao-11-php7/aula336-array-metodos-pesquisa.php
@@ -19,82 +19,24 @@
 <div class="container py-3">
 
     <header>
-        <h1>Array - Pesquisas</h1>
+        <h1 class="display-5">Array - Pesquisas</h1>
     </header>
 
     <?php
 
-        $lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
+    /**
+     * Funções para pesquisa dentro de arrays: 
+     * - in_array(): 
+     * - array_search()
+     */
 
-        echo '<pre>';
-        print_r($lista_frutas);
-        echo '</pre><hr>';
+    $lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
 
-        $lista_frutas[] = 'Abacate';
-        echo $lista_frutas[4].'<hr>';
-
-
-        // in_array() - retorna true ou false para a existência do que está sendo procurado
-        $existe = in_array('Abacate', $lista_frutas);
-
-        if ($existe) {
-            echo 'Sim, o valor pesquisado existe no array';
-        }
-        else {
-            echo 'Não, o valor pesquisado não existe no array';
-        }
-        echo '<hr>';
-
-
-        // array_search()- retorna o índice do valor pesquisado , caso ele exista
-        $existe2 = array_search('Abacaxi', $lista_frutas);
-
-        if ($existe2) {
-            echo 'Sim, o valor pesquisado existe no array';
-        }
-        else {
-            echo 'Não, o valor pesquisado não existe no array';
-        }
-        echo '<hr>';
-
-        $lista_frutas[] = 'Abacaxi';
-
-        echo '<pre>';
-        print_r($lista_frutas);
-        echo '</pre><hr>';
-
-
-        // Array multidimencional
-        $lista_coisas = [
-            'frutas' => $lista_frutas,
-            'pessoas' => ['João', 'Maria']
-        ];
-
-        echo '<pre>';
-        print_r($lista_coisas);
-        echo '</pre><hr>';
-
-        // Pesquisa em array multidimencional
-        // echo in_array('Banana', $lista_coisas['frutas']).'<br>';
-        // echo $lista_coisas['frutas'][array_search('Banana', $lista_coisas['frutas'])];
-
-        // 
-        $resultado;
-        $categoria = 'pessoas';
-        $pesquisa = 'João';
-
-        if (in_array($pesquisa, $lista_coisas[$categoria]) == 1) {
-
-            $resultado = $lista_coisas[$categoria][array_search($pesquisa, $lista_coisas[$categoria])];
-
-        }
-        else{
-            $resultado = 'O valor pesquisado não existe no array';
-        }
+    echo '<pre>';
+    print_r($lista_frutas);
+    echo '</pre>';
 
     ?>
-
-    <input type="text" value="<?= $resultado ?>" class="form-control" disabled>
 
 </div>
 

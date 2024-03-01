64e98c6a10ce9fd423f41e5f387b610b07bd5fd2
ErickFSilva <erick_ferreira@outlook.com>
2024-02-29 22:02:55 -0300

    secao-06-css3, 29/02/24 (noite)

---
+++ secao-06-css3/aula040-estilos-textos/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seção 06 - CSS3</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <div class="caixa">
+
+        <!--  -->
+
+    </div>
+
+</body>
+
+</html>

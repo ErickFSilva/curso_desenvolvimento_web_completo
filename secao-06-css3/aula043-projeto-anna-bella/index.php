45235969b691eef6e5ec17f99f0806370782f0f6
ErickFSilva <erick_ferreira@outlook.com>
2024-03-02 20:56:28 -0300

    secao-06-css3, 02/03/24 (noite)

---
+++ secao-06-css3/aula043-projeto-anna-bella/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anna Bella Oficial</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <!-- Abre principal -->
+    <div id="principal">
+
+        <!-- Abre topo -->
+        <div id="topo">
+            <img src="imagens/capa.png" alt="capa">
+        </div>
+        <!-- Fecha topo -->
+        
+        <!-- Abre menu -->
+        <div id="menu">
+            <a href="index.html">HOME</a> | 
+            <a href="biografia.html">BIOGRAFIA</a> | 
+            <a href="capanhas-publicitaria.html">CAPANHAS PUBLICITÁRIAS</a> | 
+            <a href="contato.html">CONTATO</a>
+        </div>
+        <!-- Fecha menu -->
+        
+    </div>
+    <!-- Fecha principal -->
+
+    <!-- JS -->
+    <script src="scripts.js"></script>
+
+</body>
+
+</html>

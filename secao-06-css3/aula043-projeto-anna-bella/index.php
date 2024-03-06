a93f0ff909843e3bc040c9d102dbaf805d1154d8
ErickFSilva <erick_ferreira@outlook.com>
2024-03-06 17:16:46 -0300

    secao-06-css3, 06/03/24 (tarde)

--- secao-06-css3/aula043-projeto-anna-bella/index.php
+++ secao-06-css3/aula043-projeto-anna-bella/index.php
@@ -27,12 +27,33 @@
             <a href="contato.html">CONTATO</a>
         </div>
         <!-- Fecha menu -->
+
+        <!-- Abre conteúdo -->
+        <div id="conteudo">
+            <h1>Sobre Anna Bella</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolore sint, porro doloremque hic sit odit. Ratione, possimus! Dignissimos itaque, quod hic non facere necessitatibus? Odit facilis neque omnis tenetur.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio totam vel corrupti dolor, assumenda adipisci consequuntur, sit possimus illum temporibus rem dolorum repellat neque quis facilis quas nihil sint? Excepturi?
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, reiciendis? Odit natus placeat excepturi dolore porro repellendus nobis corrupti, commodi deleniti, minus recusandae, itaque qui officiis. Distinctio, itaque debitis. Voluptate!
+            </p>
+        </div>
+        <!-- Fecha conteúdo -->
+
+        <!-- Abre rodapé -->
+        <div id="rodape">
+            <p>Todos os direitos reservados</p>
+        </div>
+        <!-- Fecha rodapé -->
         
     </div>
     <!-- Fecha principal -->
 
     <!-- JS -->
-    <script src="scripts.js"></script>
+    <!-- <script src="scripts.js"></script> -->
 
 </body>
 

91b741c045b228a124aaa5340bca2bb5b3e2685b
ErickFSilva <erick_ferreira@outlook.com>
2024-03-06 23:00:31 -0300

    secao-06-css3, 06/03/24 (noite)

--- secao-06-css3/aula043-projeto-anna-bella/biografia.php
+++ secao-06-css3/aula043-projeto-anna-bella/biografia.php
@@ -30,7 +30,7 @@
 
         <!-- Abre conteúdo -->
         <div id="conteudo">
-            <h1>Sobre Anna Bella</h1>
+            <h1>Biografia</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolore sint, porro doloremque hic sit odit. Ratione, possimus! Dignissimos itaque, quod hic non facere necessitatibus? Odit facilis neque omnis tenetur.
             </p>
@@ -38,7 +38,13 @@
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio totam vel corrupti dolor, assumenda adipisci consequuntur, sit possimus illum temporibus rem dolorum repellat neque quis facilis quas nihil sint? Excepturi?
             </p>
             <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, reiciendis? Odit natus placeat excepturi dolore porro repellendus nobis corrupti, commodi deleniti, minus recusandae, itaque qui officiis. Distinctio, itaque debitis. Voluptate!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, reiciendis? Odit natus placeat excepturi dolore porro repellendus nobis corrupti, commodi deleniti, minus recusandae, itaque qui officiis. Distinctio, itaque debitis. Voluptate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minima, similique, officia excepturi hic cumque natus maxime dolore cum eum atque magnam unde voluptatibus temporibus dignissimos, consequatur vel veniam nemo?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio culpa doloribus quisquam sequi voluptatum explicabo rem, debitis aspernatur velit ipsa eos unde, molestiae neque facere odit, similique amet! Rerum, officia.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, cumque magni iure molestias nisi error incidunt, quasi deserunt facere neque blanditiis obcaecati porro reprehenderit et? Praesentium iure molestias minus labore.
             </p>
         </div>
         <!-- Fecha conteúdo -->

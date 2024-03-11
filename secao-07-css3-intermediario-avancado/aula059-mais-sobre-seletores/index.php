34fdb93159c298de0a040596fb3ecd727f2346f9
ErickFSilva <erick_ferreira@outlook.com>
2024-03-11 16:57:47 -0300

    secao-07-css3-intermediario-avancado, 11/03/24 (tarde)

---
+++ secao-07-css3-intermediario-avancado/aula059-mais-sobre-seletores/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TecBlog - O seu blog de tecnologia</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <div id="container">
+
+        <h1>Seletores CSS</h1>
+        <h2 class="subtitulo">Exemplos de uso</h2>
+
+        <!-- Abre Área menu -->
+        <div id="area-menu">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                    <a href="index.php">Sobre</a>
+                    <a href="index.php">Contato</a>
+                </li>
+            </ul>
+        </div>
+        <!-- Fecha Área menu -->
+
+    </div>
+
+</body>
+
+</html>

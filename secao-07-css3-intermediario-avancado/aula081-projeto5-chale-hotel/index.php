396244aa326b75945eda82009c5c8bd75cf293e6
ErickFSilva <erick_ferreira@outlook.com>
2024-05-28 22:39:16 -0300

    secao-07-css3-intermediario-avancado, 28/05/24 (noite)

---
+++ secao-07-css3-intermediario-avancado/aula081-projeto5-chale-hotel/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chalé Hotel - Home</title>
+    <link rel="stylesheet" href="css/variaveis.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+
+    <!-- Abre container -->
+    <div id="container">
+    
+        <!-- Abre topo -->
+        <div id="topo">
+        
+            <!-- Abre área logo -->
+            <div id="area-logo">
+                <h1>
+                    <a href="">Chalé Hotel</a>
+                </h1>
+            </div>
+            <!-- Fecha área logo -->
+
+            <!-- Abre área manu -->
+            <div id="area-menu">
+                Menu
+            </div>
+            <!-- Fecha área menu -->
+
+        </div>
+        <!-- Fecha topo -->
+
+    </div>
+    <!-- Fecha container -->
+
+</body>
+
+</html>

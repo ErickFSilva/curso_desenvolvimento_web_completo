2c1e1e3f50e96480d5180e83e01ed3a22fe20b87
ErickFSilva <erick_ferreira@outlook.com>
2024-06-06 16:50:19 -0300

    secao-08-html5-css3_&_secao-10-javascript_06/06/24 (tarde)

---
+++ secao-10-javascript/aula154-incluindo-javascript-paginas-html/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Curso de Javascript</h1>
+    </header>
+
+    <!-- JS -->
+    <script>
+        alert('Olá, mundo!');
+    </script>
+
+    <script src="script.js"></script>
+
+</body>
+
+</html>

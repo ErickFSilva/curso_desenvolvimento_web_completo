7469b03fd6581f1743fd7365ea9e4b4729a217fa
ErickFSilva <erick_ferreira@outlook.com>
2024-06-10 15:57:08 -0300

    secao-08-html5-css3, 10/06/24 (tarde)

---
+++ secao-08-html5-css3/aula103-projeto6-museu-nacional/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Museu Nacional</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/variaveis.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+
+    <!-- Abre container -->
+    <div id="container">
+
+        <!-- Abre header (cabecalho) -->
+        <header>
+
+            <!-- Abre logo -->
+            <div id="logo">
+                <h1>
+                    <a href="index.php">Museu Nacional</a>
+                </h1>
+            </div>
+            <!-- Abre logo -->
+
+            <!-- Abre navegação -->
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php">Exposições</a></li>
+                    <li><a href="index.php">Pesquisa</a></li>
+                    <li><a href="index.php">Acervo</a></li>
+                    <li><a href="index.php">Vídeos</a></li>
+                    <li><a href="index.php">Fotos</a></li>
+                    <li><a href="index.php">Contato</a></li>
+                </ul>
+            </nav>
+            <!-- Abre navegação -->
+
+        </header>
+        <!-- Fecha header (cabecalho) -->
+
+        <!-- Abre principal -->
+        <div id="principal">
+
+            <!-- Abre conteúdo -->
+            <div id="conteudo">
+                Conteúdo
+            </div>
+            <!-- Fecha conteúdo -->
+
+            <!-- Abre lateral -->
+            <aside>
+                Lateral
+            </aside>
+            <!-- Fecha lateral -->
+
+        </div>
+        <!-- Fecha principal -->
+
+    </div>
+    <!-- Fecha container -->
+
+</body>
+
+</html>

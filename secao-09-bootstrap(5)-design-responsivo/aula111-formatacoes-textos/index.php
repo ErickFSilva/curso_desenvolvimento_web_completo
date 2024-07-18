11a059bc836ea059f0cd28b278b0723a517572e2
ErickFSilva <erick_ferreira@outlook.com>
2024-07-18 16:22:16 -0300

    secao-09-bootstrap(5)-design-responsivo, 18/07/24 (tarde)

---
+++ secao-09-bootstrap(5)-design-responsivo/aula111-formatacoes-textos/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <!-- Bootstrap-5.3.3 (local) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border">
+                <!-- Cabeçalhos -->
+                <h1 class="h1">Cabeçalho 1</h1>
+                <p class="h2">Cabeçalho 2</p>
+                <h1 class="h3">Cabeçalho 3</h1>
+                <div class="h4">Cabeçalho 4</div>
+                <h1 class="h5">Cabeçalho 5</h1>
+                <span class="h6">Cabeçalho 6</span>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border">
+                <!-- Display classes - Maior destaque -->
+                <h1 class="display-1">Display 1</h1>
+                <h1 class="display-2">Display 2</h1>
+                <h1 class="display-3">Display 3</h1>
+                <h1 class="display-4">Display 4</h1>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border">
+                <!-- Parágrafos -->
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut perspiciatis autem quae laboriosam, reprehenderit impedit blanditiis officiis dolore beatae eos necessitatibus ab aspernatur, ipsum sit veritatis distinctio ex provident placeat.
+                </p>
+
+                <p class="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus in at mollitia voluptatem, fugit, illo corrupti ipsa similique eligendi assumenda delectus quia hic? Error corporis earum harum est distinctio!
+                </p>
+
+                <p class="font-monospace">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, inventore ad, quisquam vitae eum aspernatur nobis velit nulla dicta in consequatur sapiente ipsa ullam voluptates at officia veritatis laudantium vel?
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border">
+                <!-- Classe de estilo -->
+                <p class="fw-bold">Texto negrito</p>
+                <p class="fw-normal">Texto normal</p>
+                <p class="fst-italic">Texto itálico</p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border">
+                <!-- Transformação de textos -->
+                <p class="text-uppercased">Letras maiúsculas</p>
+                <p class="text-lowercase">LETRAS MINÚSCULAS</p>
+                <p class="text-capitalize">primeira letra maiúscula</p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border"></div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border"></div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border"></div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border"></div>
+
+            <div class="col-md-6 col-lg-4 col-xxl-3 border"></div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>

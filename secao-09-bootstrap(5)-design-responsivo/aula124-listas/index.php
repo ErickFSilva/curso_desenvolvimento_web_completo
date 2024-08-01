5888c30276b801ea3dca242443c5ff20d9e446fe
ErickFSilva <erick_ferreira@outlook.com>
2024-08-01 15:55:18 -0300

    secao-09-bootstrap(5)-design-responsivo, 01/08/24 (tarde)

---
+++ secao-09-bootstrap(5)-design-responsivo/aula124-listas/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+
+    <!-- Bootstrap 5.3.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+
+            <h1 class="display-4 border-bottom border-dark my-3 pb-3">Grupo de Listas</h1>
+
+            <!-- Listas -->
+            <div class="col-12 p-2 mb-3 border">
+                <h2 class="h3 mb-3 px-2 pb-2 text-secondary border-bottom border-light" id="ancora-1">Listas:</h2>
+
+                <ul class="list-group">
+                    <li class="list-group-item">Meu item de lista 1</li>
+                    <li class="list-group-item">Meu item de lista 2</li>
+                    <li class="list-group-item">Meu item de lista 3</li>
+                    <li class="list-group-item">Meu item de lista 4</li>
+                </ul>
+            </div>
+
+            <!-- Listas com classe flush -->
+            <div class="col-12 p-2 mb-3 border">
+                <h2 class="h3 mb-3 px-2 pb-2 text-secondary border-bottom border-light" id="ancora-2">Listas com classe flush:</h2>
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Meu item de lista 1</li>
+                    <li class="list-group-item">Meu item de lista 2</li>
+                    <li class="list-group-item">Meu item de lista 3</li>
+                    <li class="list-group-item">Meu item de lista 4</li>
+                </ul>
+            </div>
+
+            <!--  -->
+            <div class="col-12 p-2 mb-3 border">
+                <h2 class="h3 mb-3 px-2 pb-2 text-secondary border-bottom border-light" id="ancora-2">:</h2>
+
+                <ul>
+                    <li>Meu item de lista 1</li>
+                    <li>Meu item de lista 2</li>
+                    <li>Meu item de lista 3</li>
+                    <li>Meu item de lista 4</li>
+                </ul>
+            </div>
+
+            <!--  -->
+            <div class="col-12 p-2 mb-3 border">
+                <h2 class="h3 mb-3 px-2 pb-2 text-secondary border-bottom border-light" id="ancora-2">:</h2>
+
+                <ul>
+                    <li>Meu item de lista 1</li>
+                    <li>Meu item de lista 2</li>
+                    <li>Meu item de lista 3</li>
+                    <li>Meu item de lista 4</li>
+                </ul>
+            </div>
+
+            <!--  -->
+            <div class="col-12 p-2 mb-3 border">
+                <h2 class="h3 mb-3 px-2 pb-2 text-secondary border-bottom border-light" id="ancora-2">:</h2>
+
+                <ul>
+                    <li>Meu item de lista 1</li>
+                    <li>Meu item de lista 2</li>
+                    <li>Meu item de lista 3</li>
+                    <li>Meu item de lista 4</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
